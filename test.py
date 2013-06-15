edba8fda41a7bad06f03eecd844941553b930584
Andrew Boltachev <andrewboltachev@gmail.com>
2013-06-15 13:37:11 +0400

    Сделал скрипт для тильды для GD и тесты к нему

---
+++ test.py
@@ -0,0 +1,90 @@
+# -*- coding=UTF-8 -*-
+
+from __future__ import unicode_literals
+
+import unittest
+
+import os
+
+import StringIO
+
+# import filecmp
+
+from goldendict_reader import GoldenDictReader
+from sort import GoldenDictSorter
+from tilde import GoldenDictTildeProcessor
+# last line of imports
+
+
+class GoldenDictScriptsTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.filename = lambda x: os.path.join(
+            os.path.dirname(__file__), 'files', x)
+
+    def test_dict_reader_works_well(self):
+        stdout = StringIO.StringIO()
+        stderr = StringIO.StringIO()
+        filename = self.filename('original_utf8.dsl')
+        original_file = open(filename)
+        original = [line for line in original_file]
+        original_file.close()
+        GoldenDictReader()(
+            ['sort.py', filename],
+            stdout=stdout,
+            stderr=stderr
+        )
+        self.maxDiff = None
+        stdout.seek(0)
+        self.assertEqual(original, stdout.readlines())
+
+    def test_dict_sorter_works_well(self):
+        stdout = StringIO.StringIO()
+        stderr = StringIO.StringIO()
+        original_file = open(self.filename('original_utf8.dsl'))
+        original = [line for line in original_file]
+        original_file.close()
+        GoldenDictSorter()(
+            ['sort.py', self.filename('unsorted_utf8.dsl')],
+            stdout=stdout,
+            stderr=stderr
+        )
+        self.maxDiff = None
+        stdout.seek(0)
+        '''
+        for x in stdout.readlines():
+            print x
+        stdout.seek(0)
+        '''
+        self.assertEqual(original, stdout.readlines())
+
+    def test_tilde_processor_works_well(self):
+        stdout = StringIO.StringIO()
+        stderr = StringIO.StringIO()
+        original_file = open(self.filename('tilde_processed_utf8.dsl'))
+        original = [line for line in original_file]
+        original_file.close()
+        GoldenDictTildeProcessor()(
+            ['sort.py', self.filename('original_utf8.dsl')],
+            stdout=stdout,
+            stderr=stderr
+        )
+        self.maxDiff = None
+        stdout.seek(0)
+        '''
+        for x in stdout.readlines():
+            print x
+        stdout.seek(0)
+        '''
+        self.assertEqual(original, stdout.readlines())
+
+    def tearDown(self):
+        pass
+        '''
+        filename = self.filename('result_utf8.dsl')
+        if os.path.exists(filename):
+            os.unlink(filename)
+        '''
+
+if __name__ == '__main__':
+    unittest.main()
--- tilde.py
+++ tilde.py
@@ -7,37 +7,16 @@ from goldendict_reader import GoldenDictReader
 
 class GoldenDictTildeProcessor(GoldenDictReader):
 
-    def _sort(self, articles, is_lower=False):
-        do_lower = lambda str, is_lower: str.lower() if is_lower else str
-        alphabet = 'аӓбвгдеёжӝзӟиіӥйклмнҥоӧпрстуӱфхцчӵшщъыӹьэюя'
-        if not is_lower:
-            alphabet += alphabet.upper()
-        alphabet_dict = dict([(x, alphabet.index(x)) for x in alphabet])
-
-        def only_alphabet_chars(str, alphabet):
-            for letter in str:
-                if not letter in alphabet:
-                    str = str.replace(letter, '')
-            return str
-
-        articles = sorted(
-            articles,
-            key=lambda word: [
-                alphabet_dict[c] for c in only_alphabet_chars(
-                    do_lower(
-                        word['name'],
-                        is_lower
-                    ),
-                    alphabet
-                )
-            ]
-        )
-        return articles
+    def _procces_tilde(self, x):
+        text = x['text'].replace('~', x['name'])
+        return {
+            'name': x['name'],
+            'text': text,
+        }
 
     def process(self, articles):
 
-        articles = self._sort(articles, False)
-        articles = self._sort(articles, True)
+        articles = [self._procces_tilde(x) for x in articles]
 
         return articles
 
