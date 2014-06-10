7a93d0e558d5adff2db023c5937cde68d675f588
Andrey Chemyshev <chemyshev.andrey@gmail.com>
2014-06-10 11:48:30 +0400

    эти скрипты тоже нужно нверное подправить

---
+++ file0002.py
@@ -0,0 +1,11 @@
+f1 = open('mhr2.txt')
+f2 = open('mhr1.txt')
+a1 = []
+a2 = []
+for l in f1:
+	a1.append(l.decode('utf-8'))
+for l in f2:
+	a2.append(l.decode('utf-8'))
+for l in a2:
+	if not l in a1:
+		print l.encode('utf-8')
